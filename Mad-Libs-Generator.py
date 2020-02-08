24a4e9932a4e0dfcf697dcbadcacb5917e33db14
Mega-Barrel <saurabhjoshi7869@gmail.com>
2020-02-08 22:48:48 +0530

    initial commit

---
+++ Mad-Libs-Generator.py
@@ -0,0 +1,28 @@
+'''
+Project 3: Mad Libs Generator
+
+The Goal: 
+The program will first prompt the user for a series of inputs a la Mad Libs. 
+For example, a singular noun, an adjective, etc. 
+Then, once all the information has been inputted, the program will take that data and place them 
+into a premade story template. You’ll need prompts for user input,
+and to then print out the full story at the end with the input included.
+
+Concepts to keep in mind:
+
+1. Strings
+2. Variables
+3. Concatenation
+4. Print
+
+'''
+
+
+
+
+
+
+
+
+
+
