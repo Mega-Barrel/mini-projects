3c747ed0ba01c2924e9c3450f11ba18898cd813d
Mega-Barrel <saurabhjoshi7869@gmail.com>
2020-02-16 19:11:50 +0530

    initial commit

---
+++ anagram.py
@@ -0,0 +1,10 @@
+'''
+Project 5 : Anagram 
+
+Make a anagram validator.
+Enter a string and check if the string is anagram or not. 
+
+If the string is not anagram print("Not a anagram")
+
+If the string is anagram print("Anagram")
+''' 
