08fe65b35753e7c6ebf1266cf446443e2f03616f
Mega-Barrel <saurabhjoshi7869@gmail.com>
2020-02-17 20:29:05 +0530

    Added code

--- anagram.py
+++ anagram.py
@@ -8,3 +8,24 @@ If the string is not anagram print("Not a anagram")
 
 If the string is anagram print("Anagram")
 ''' 
+
+# a = input('Enter 1st sentence or integer: ')
+# b = input('Enter 2nd sentence or integer: ')
+
+while True:
+    def anagram_checker():
+        a = input('Enter 1st sentence or integer: ')
+        b = input('Enter 2nd sentence or integer: ')
+        if sorted(a) == sorted(b):
+            print('<-Anagram->')
+        else:
+            print('<-Not a Anagram->')
+
+    
+    play_again = input('Do you want to play Anagram Checker: ')
+    if play_again == 'no':
+        print('Thank you for playing Anagram checker :)')
+        break
+    elif play_again == 'yes':
+        anagram_checker()
+
