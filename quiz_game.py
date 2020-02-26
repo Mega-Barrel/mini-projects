8f14f1feee94d4b91d23d37787c62c8eaa3dab1c
Mega-Barrel <saurabhjoshi7869@gmail.com>
2020-02-26 18:19:58 +0530

    deleted quiz game

--- quiz_game.py
+++
@@ -1,9 +0,0 @@
-'''
-Project 6 : quiz program 
-
-Make a multiple choice quiz program.
-The question will contain 4 options.
-In this user has to choose only 1 option from the answers. 
-If his answer is correct, then it will print correct answer 
-Else it will print incorrect answer.
-''' 
